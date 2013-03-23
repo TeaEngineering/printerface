f65cdf9960031a1c3aa4fc5888ffae2f564c8520
Chris Shucksmith <chris@shucksmith.co.uk>
2013-03-23 16:45:55 +0000

    lpd works, merged with HTTP poller

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ printerface
 ===========
 
 LPD to email gateway
+
+`lpdserver.py` implements a UNIX LPD daemon for recieving print jobs
+`httpserver.py` implements a http status page
---
+++ lpdserver.py
@@ -0,0 +1,119 @@
+
+
+import asynchat, asyncore, socket
+
+(AwaitingCommand, NewJob, NewJobControl, NewJobData, EndOfFile ) = range(0,5)
+
+class LpdHandler(asynchat.async_chat):
+
+	def __init__(self, sock, addr, controller):
+		asynchat.async_chat.__init__(self, sock=sock)
+		self.ibuffer = []
+		self.sm = AwaitingCommand
+		self.set_terminator("\n")
+		self.sock = sock
+		self.controller = controller
+		self.id = "[lpd-%d]" % addr[1]
+		print '%s installed LpdHandler' % self.id
+				
+	def collect_incoming_data(self, data):
+		self.ibuffer.append(data)
+
+	def found_terminator(self):
+		chunk = self.ibuffer.pop(0)
+		if self.sm == AwaitingCommand: # Main command verb
+			if chunk[0] == '\x02':
+				self.queue = chunk[1:]
+				print '%s Incoming job to queue \'%s\'' % (self.id, self.queue)
+				self.push('\x00')
+				self.sm = NewJob
+			else:
+				print '%s unknown message type %d' + (self.id, self.ibuffer[0])
+		elif self.sm == NewJob: # Recieve Job
+			if chunk[0] == '\x02':	# Subcommand - Control filename
+				sz,name = chunk[1:].split(' ', 1)
+				rdlength = int(sz)
+				print '%s Incoming header: control file sz %d filename %s' % (self.id, rdlength,name)
+				self.sm = NewJobControl
+				self.set_terminator(rdlength+1) # include the trailing null
+				self.push('\x00')				
+			if chunk[0] == '\x03': # Subcommand - Data filename
+				sz,name = chunk[1:].split(' ', 1)
+				rdlength = int(sz)
+				print '%s incoming header: data file sz %d filename %s' % (self.id, rdlength,name)
+				self.sm = NewJobData
+				# windows seems to set a stupidly large length here and kill the socket to 
+				# indicate eof. Handle either
+				self.set_terminator(rdlength+1) # include the trailing null
+				# self.set_terminator(None) # terminated by close of socket
+				self.push('\x00')
+		elif self.sm == NewJobControl: # Recieve - Job Control file (null terminated)
+			self.controldata = dict([ (x[0], x[1:]) for x in chunk[:-1].split('\n') if len(x) > 1])
+			print '%s control file: %s' % (self.id, repr(self.controldata))
+			self.push('\x00')
+			self.sm = NewJob
+			self.set_terminator("\n")
+		elif self.sm == NewJobData: # Recieve - Job Data file (terminated by close)
+			print '%s end of data file!' % self.id
+			self.dispatch()
+			self.sm = NewJob
+		else:
+			print '%s Unknown state, woken up by terminator, buffer was: %s' % (self.id, repr(self.ibuffer))
+			self.close_when_done()
+	def handle_close(self):
+		
+		if self.sm == NewJobData:
+			print '%s data file: %s' % (self.id, repr(self.ibuffer))
+			# calling push() after the client has closed the socket will re-invoke handle_close, so 
+			# switch to terminal state to avoid inf recursion
+			self.sm = EndOfFile
+			self.dispatch()
+			self.push('\x00')
+			self.close_when_done()
+		elif self.sm == EndOfFile:
+			self.close()
+		else:
+			print '%s unexpected close of stream, sm=%d' % (self.id, self.sm)
+			self.close()
+
+	def dispatch(self):
+		data = ''.join(self.ibuffer)
+		self.controller(self.queue, self.sock.getpeername(), self.sock.getsockname(), self.controldata, data)
+		print '%s dispatched' % (self.id)
+
+class LpdServer(asyncore.dispatcher):
+	def __init__ (self, lpd_controller, ip='127.0.0.1', port=515):
+		asyncore.dispatcher.__init__ (self)
+		self.create_socket (socket.AF_INET, socket.SOCK_STREAM)
+		self.set_reuse_addr()
+		self.bind ((ip, port))
+		self.listen (100)
+		self.lpd_controller = lpd_controller
+		print "[lpd] listening on port %s" % port
+
+	def handle_accept(self):
+		try:
+			pair = self.accept()
+			if pair is not None:
+				sock, addr = pair
+				print '[lpd] Incoming connection from %s' % repr(addr)
+				LpdHandler(sock, addr, self.lpd_controller)
+		except socket.error:
+			print '[lpd] warning: server accept() threw an exception'
+		except TypeError:
+			print '[lpd] warning: server accept() threw EWOULDBLOCK'
+
+def jobPrinter(queue, local, remote, control, data):
+	print "    %s" % queue
+	print "    %s" % repr(local)
+	print "    %s" % repr(remote)
+	print "    %s" % control
+	print "    %s" % repr(data)
+
+if __name__=="__main__":
+	# launch the server on the specified port
+	s = LpdServer(jobPrinter, ip='192.168.1.75', port=515)
+	try:
+		asyncore.loop(timeout=2)
+	except KeyboardInterrupt:
+		print "Crtl+C pressed. Shutting down."
