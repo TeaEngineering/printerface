fdf64744d167c62e8042328deac0e48b502e15ba
Chris Shucksmith <chris@shucksmith.co.uk>
2019-09-16 10:04:23 +0100

    handle no lpstat in path

--- printing.py
+++ printing.py
@@ -9,13 +9,15 @@ def getPrinters():
 	if not sys.platform == "linux2":
 		return ['default']
 	if len(printers) > 0: return printers
-	
-	process = subprocess.Popen(["lpstat", "-a"], stdout=subprocess.PIPE)
-	result = process.communicate()[0].strip()
-	# KONICA_bizhub_192.168.12.10 accepting requests since Sun 16 Dec 2012 07:43:59 PM GMT
-	print(result)
-	printers = [x.split(' ')[0] for x in result.split('\n')]
-	print('[print] printers=%s' % repr(printers))
+	try:
+		process = subprocess.Popen(["lpstat", "-a"], stdout=subprocess.PIPE)
+		result = process.communicate()[0].strip()
+		# KONICA_bizhub_192.168.12.10 accepting requests since Sun 16 Dec 2012 07:43:59 PM GMT
+		print(result)
+		printers = [x.split(' ')[0] for x in result.split('\n')]
+		print('[print] printers=%s' % repr(printers))
+	except OSError as e:
+		print('[print] %s' % repr(e))
 	return printers
 
 def printFile(file, printer):
